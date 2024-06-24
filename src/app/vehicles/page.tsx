b322bdb4cde969c907b55619260533e68b69a177
Jeff Hogg <jeff@hoggs.io>
2024-06-24 16:21:39 -0500

    Revert "add vehicle page"

--- src/app/vehicles/page.tsx
+++ src/app/vehicles/page.tsx
@@ -1,35 +1,15 @@
 'use client'
-
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
+import VehicleList from '@/app/vehicles/VehicleList';
 import { Container } from '@/components/Container';
-import Link from 'next/link';
-import { Card } from '@/components/Card';
-import Vehicle from '@/lib/vehicle'
+import Image from 'next/image';
 
 const VehiclePage = () => {
   const [searchQuery, setSearchQuery] = useState('');
-  const [vehicles, setVehicles] = useState<Vehicle[]>([]);
-
-  useEffect(() => {
-    async function fetchVehicles() {
-      try {
-        const response = await fetch('/api/vehicles');
-        const data: Vehicle[] = await response.json();
-        setVehicles(data);
-      } catch (error) {
-        console.error('Error fetching vehicles:', error);
-      }
-    }
-    fetchVehicles();
-  }, []); // Empty dependency array ensures this runs only once on component mount
-
-  const filteredVehicles = vehicles.filter((vehicle) =>
-    vehicle.car_name.toLowerCase().includes(searchQuery.toLowerCase())
-  );
 
   return (
-    <Container className="m-9 pt-6">
-      <section className="m-8">
+    <Container className="mt-9">
+      <section className="mb-8">
         <div className="relative"> 
           <input 
             type="text"
@@ -47,24 +27,9 @@ const VehiclePage = () => {
         </div>
       </section>
 
-      <ul
-        role="list"
-        className="grid grid-cols-1 gap-x-8 gap-y-12 sm:grid-cols-2 lg:grid-cols-3"
-      >
-        {filteredVehicles.map((vehicle) => (
-          <Link 
-            href={`/vehicles/${vehicle.car_name}`} // Assuming you have an 'car_name' property for each vehicle
-            key={vehicle.car_name}
-            className="block" // Makes the entire card clickable
-          > 
-            <Card className="rounded-xl overflow-hidden shadow-md"> 
-              {/* ... (Rest of the card content from VehicleList remains the same) */}
-            </Card>
-          </Link>
-        ))}
-      </ul>
+      <VehicleList searchQuery={searchQuery} />
     </Container>
   );
 }
 
-export default VehiclePage;
+export default VehiclePage
