c4f868c8402ab6123a144cf051ad7369bb8f6485
Surya Polina <suryapolina@gmail.com>
2024-12-06 10:15:19 -0600

    db.ts

--- src/utils/db/db.ts
+++ src/utils/db/db.ts
@@ -28,20 +28,19 @@ export async function fetchAvailableVehicles(startDate: string, endDate: string)
 
 export async function fetchVehicleById(id: number): Promise<Vehicle | null> {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  return await fetchWithToken(`${baseURL}/api/vehicle/${id}`);
+  return await fetchWithToken(`/api/vehicle/${id}`);
 }
 
 export async function fetchImagesByVehicleId(vehicle_id: number): Promise<VehicleImages[] | null> {
-  const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  return await fetchWithToken(`${baseURL}/api/vehicle_images/${vehicle_id}`);
+  return await fetchWithToken(`/api/vehicle_images/${vehicle_id}`);
 }
 
 export async function fetchBookingById(id: string, token?: string) {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  return await fetchWithToken(`${baseURL}/api/booking/${id}`, token);
+  return await fetchWithToken(`/api/booking/${id}`, token);
 }
 
 export async function fetchUserById(token?: string) {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
-  return await fetchWithToken(`${baseURL}/api/user`, token);
+  return await fetchWithToken(`/api/user`, token);
 }
